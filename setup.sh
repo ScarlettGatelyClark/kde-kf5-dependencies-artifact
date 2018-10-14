b0309d0a4c76c47677e80790197bc67753eaaf95
Scarlett Gately Moore <sgclark@kde.org>
2018-10-14 14:54:12 -0700

    Su into jenkins in container.

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+su jenkins
 export PATH=/opt/usr/bin:~/.rbenv/bin:~/.rbenv/shims:$PATH
 export WORKSPACE=`pwd`
 echo $WORKSPACE
